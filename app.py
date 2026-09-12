f005d08b8d614f0609e5ab013429706ea42dcf0d
engrbkakar-glitch <engr.bkakar@gmail.com>
2026-09-12 23:10:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ data = boq.merge(progress, on="item_id")
 physical_progress = (
     data["physical_progress"] * data["weight"] / 100
 ).sum()
+financial_progress = (
+    data["financial_progress"] * data["weight"] / 100
+).sum()
 # Page configuration
 st.set_page_config(
     page_title="Construction Progress Monitor",
